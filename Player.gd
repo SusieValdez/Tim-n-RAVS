116bacb7705bb8ef1e3241d78381ce7e0a9f780f
Psiloclast <me@psiloclast.com>
2021-09-15 05:37:27 -0500

    fix: player animations

--- Player.gd
+++ Player.gd
@@ -3,59 +3,41 @@ extends KinematicBody2D
 const SPEED = 400
 const GRAVITY = 35
 const JUMPFORCE = -700
-const MAX_SPEED = 500
-const ACCELERATION = 600
-const FRICTION = 500
-
 
 onready var sprite = $Sprite
-onready var animationPlayer = $AnimationPlayer
-onready var animationTree = $AnimationTree
-onready var animationState = animationTree.get("parameters/playback")
 
-var velocity = Vector2.ZERO
+var velocity = Vector2(0,0)
 var can_jump = false
+var direction = "Right"
 
-enum {
-	MOVE, 
-}
-
-var state = MOVE
-
-func _process(delta):
-	match state:
-		MOVE:
-			move_state(delta)
-
-func move_state(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	input_vector = input_vector.normalized()
-	
-	if input_vector != Vector2.ZERO:
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+func _physics_process(_delta):
+	if Input.is_action_pressed("right"):
+		direction = "Right"
+		velocity.x = SPEED
+	elif Input.is_action_pressed("left"):
+		direction = "Left"
+		velocity.x = -SPEED
 	else:
-		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-		if can_jump and Input.is_action_just_pressed("jump"):
-			velocity.y = JUMPFORCE
-	
+		velocity.x = 0
+
 	velocity.y = velocity.y + GRAVITY
 	if is_on_floor():
 		can_jump = true
 	if not is_on_floor() and $JumpTimer.is_stopped():
 		$JumpTimer.start()
-	
-	move()
-	
+	if can_jump and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMPFORCE
+		
+	if is_on_floor():
+		if velocity.x == 0:
+			$AnimationPlayer.play("Idle" + direction)
+		else:
+			$AnimationPlayer.play("Run" + direction)
+	else:
+		$AnimationPlayer.play("Jump" + direction)
 
-func move():
-	velocity = move_and_slide(velocity)
-
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(velocity.x, 0, 0.2)
 
 func _on_JumpTimer_timeout():
 	can_jump = false
