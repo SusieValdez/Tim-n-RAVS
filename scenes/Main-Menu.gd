a3d85d845df085b442ae8f5038931b99cdbb2f0b
Psiloclast <me@psiloclast.com>
2021-09-17 07:20:44 -0500

    wiggle title

--- scenes/Main-Menu.gd
+++ scenes/Main-Menu.gd
@@ -1,13 +1,4 @@
 extends Control
 
-var viewportWidth = get_viewport().size.x
-var viewportHeight = get_viewport().size.y
-
-onready var Background = $Sprite
-
-var scale = viewportWidth / Background.texture.get_size().x
-
-# Set same scale value horizontally/vertically to maintain aspect ratio
-# If however you don't want to maintain aspect ratio, simply set different
-# scale along x and y
-# Background.set_scale(Vector2(scale, scale))
+func _ready():
+	$AnimationPlayer.play("TitleDance")
