949172f46937abd0ec4f039dff34ed5204ff5ed4
Psiloclast <me@psiloclast.com>
2021-09-21 03:57:13 -0500

    feat: add level 4, fix flipped converyor belts and race condition in cog

--- entities/Capsule.gd
+++ entities/Capsule.gd
@@ -2,14 +2,18 @@ extends Sprite
 
 const ROTATION_SPEED = 5
 
+# Prevents race condition where capsule is captured multiple times
+var captured = false
+
 func _physics_process(delta):
 	rotation += delta * ROTATION_SPEED
 
 func _on_Area2D_body_entered(body):
-	if body is Player:
+	if body is Player and not captured:
 		Globals.add_capsule()
 		Globals.random_child($Sounds).play()
 		visible = false
+		captured = true
 
 func _on_PickupCapsuleAudio_finished():
 	queue_free()
--- entities/ConveyorBelt.gd
+++ entities/ConveyorBelt.gd
@@ -1,10 +1,11 @@
 extends StaticBody2D
 
 export var flipped = false
-var speed_offset = 250 if flipped else -250
+var speed_offset
 
 func _ready():
 	$AnimatedSprite.flip_h = flipped
+	speed_offset = 250 if flipped else -250
 
 func _on_Area2D_body_entered(body):
 	if body is Player:
